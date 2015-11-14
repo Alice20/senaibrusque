f699c2e01d245f4a49ba8110b3edd2c035fd883b
Alice20 <alii.alice@hotmail.com>
2015-11-13 23:00:39 -0200

    closes #4

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html lang="pt-br">>
+<html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <title>Site Institucional do SENAI - Brusque</title>
+         <meta charset="UTF-8">
+         <title>Site Institucional do SENAI - Brusque</title>
+        
+            <link rel="stylesheet" href="css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="css/estilo.css"/>
+
+        
+            <!-- Javascript -->
+            <script src="js/ jquery-1.11.3.min.js"></script> 
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/>meuscript.js"></script>
+                
+        
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+        <body>
+                
+            <div class="container">
+                <header class="jumbotron text-center" id="topo">
+                    <h1>SENAI/SC Brusque</h1>
+                    <p>Alo Ha mundo do Bootstrap</p>
+                        
+                        <form role="form">
+                                
+                            <div class="form-inline">
+                                
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-addon">
+                                        <span class="glypicon glyphicon-envelope"></span>
+                                    </span>
+                                  
+                                <input type="email" class="form-control" id="email" placeholder="E-mail"/>
+                             
+                                 </div>    
+                            
+                            </div>
+                                                        
+                               <button type="submit" class="btn btn-success">Cadastrar</button>
+                            
+                        </form>
+                                        
+                         
+                </header>
+                
+            </div>
+            
+        </body>
 </html>
