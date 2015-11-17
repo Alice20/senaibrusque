77efb44d69259f27dee9c38c2f53e748e81425aa
alice20 <alii.alice@hotmail.com>
2015-11-16 23:04:01 -0200

    #18

--- index.php
+++ index.php
@@ -52,14 +52,14 @@
                             <li><a href="#section5">Vídeos</a></li>
                             <li><a href="#section6">Contato</a></li>
                             <li><a href="#section7">Localização</a></li>
-                        <!-- 
-                           <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#section41">Section 4-1</a></li>
-                                    <li><a href="#section42">Section 4-2</a></li>
-                                </ul>
-                            </li>
-                        -->
+                            <!-- 
+                               <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#section41">Section 4-1</a></li>
+                                        <li><a href="#section42">Section 4-2</a></li>
+                                    </ul>
+                                </li>
+                            -->
                         </ul>
                     </div>
                 </div>
@@ -68,8 +68,45 @@
 
         <div id="section1" class="container-fluid">
             <h1>Home</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="img_chania.jpg" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="img_chania2.jpg" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="img_flower.jpg" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="img_flower2.jpg" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
         </div>
         <div id="section2" class="container-fluid">
             <h1>Cursos</h1>
